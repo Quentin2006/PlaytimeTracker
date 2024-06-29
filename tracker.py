32c07f832f8de8b05083e08acefd26b69679af43
Quentin Scheetz <124484154+Quentin2006@users.noreply.github.com>
2024-06-28 19:47:38 -0700

    program that work with more than one .exe

--- tracker.py
+++ tracker.py
@@ -2,40 +2,46 @@ import time     # time.sleep()
 import psutil   # to check if the .exe is running
 import os
 
-# CURRENTLY ONLY WORKS FOR ONE GAME, WILL MAKE IT WORK FOR MORE 
+game_names = ["GhostOfTsushima", "Hades"]   # List of game executable names (without .exe)
 
-game_names = ["GhostOfTsushima"]   # MUST BE NAME OF EXE - THE .exe EXTENSION
+tracked_files = {game: f"{game}.txt" for game in game_names}
 
-increment_var = 10   # decides how much program should count by, lower numbers result in better performance
+increment_var = 10   # Time increment in seconds, the higher the # the better the performance 
 
 def main():
     while True:
         time.sleep(increment_var)
-        for process in psutil.process_iter(["name"]):   # gets all processes currently running
-            for game in game_names:                     # gets all games in list
-                if process.info["name"] == game + ".exe":
-                    create_and_write_to_file(game)          # makes file and tracks playtime
-                    print("running")
+        running_games = get_running_games()
+        for game in running_games:
+            update_playtime(game)
+            print(f"{game} is running")
+
+def get_running_games():
+    running_games = set()
+    for process in psutil.process_iter(["name"]):   # gets all processes currently running
+        process_name = process.info["name"]
+        for game in game_names:                 # gets all games in list
+            if process_name == game + ".exe":
+                running_games.add(game)
+    return running_games
 
 
-def create_and_write_to_file(game):
-    file_name = game + ".txt"
-
+def update_playtime(game):
+    file_name = tracked_files[game]
+    
     # if file does not exist, it will create file and start timer at designated increment variable defined above
     if not os.path.exists(file_name):
         with open(file_name, 'w') as file:
-            file.write(str(increment_var))
+            file.write(str(0))
 
     # opens file in read mode to get contents of file
     with open(file_name, 'r') as file:
-        content = file.read()
-
-    time = int(content) + increment_var
+        content = int(file.read().strip())
+        playtime = content + increment_var
 
     # opens file in write mode to update play time
     with open(file_name, 'w') as file:
-        file.write(str(time))
+        file.write(str(playtime))
 
-    return None
-
-main()
+if __name__ == "__main__":
+    main()
