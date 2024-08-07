b02e01070a18c4d93a083f046cfbea33bfc0a9a0
Quentin Scheetz <124484154+Quentin2006@users.noreply.github.com>
2024-08-07 11:12:29 -0700

    Returns webpage for game img, using steam API for better performace
    Didnt need the entire img file, only needed the webpage.

--- game_icon.py
+++ game_icon.py
@@ -1,44 +1,18 @@
-import json
-from urllib.request import urlopen
-import requests
-import os
+from steam_web_api import Steam
+from tracker import game_names
+from secret import api_key
 
-# Define the URL for the Steam API endpoint
-url = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
+def getURLs():
+    URL = 'https://api.steampowered.com/ISteamApps/GetAppList/v2/'
+    steam = Steam(api_key)
 
-# Fetch data from the URL
-response = urlopen(url)
+    imgURLs = []
+    for game in game_names:
+        output = steam.apps.search_games(game)
+        id = output["apps"][0]["id"][0]
+        imgURL = "https://cdn.cloudflare.steamstatic.com/steam/apps/" + str(id) + "/library_600x900_2x.jpg"
+        imgURLs.append(imgURL)
 
-# Load JSON data from the response
-data = json.load(response)
+    return(imgURLs)
 
-# Simulating your games_list function
-def get_game_list():
-    return ["Ghost of Tsushima DIRECTOR'S CUT", "Forza Horizon 5"]
-
-games_list = get_game_list()
-
-# Extract and print the app list
-app_list = data['applist']['apps']
-
-for app in app_list:
-    for game in games_list:
-        if game == app['name']:
-            image_url = f"https://cdn.cloudflare.steamstatic.com/steam/apps/{app['appid']}/library_600x900_2x.jpg"
-            filename = os.path.join('static', 'icon', f"{game}.jpg")
-            if not os.path.exists(os.path.join('static', 'icon')):
-                os.makedirs(os.path.join('static', 'icon'))
-            try:
-                # Send a GET request to download the image
-                image_response = requests.get(image_url)
-
-                # Check if the request was successful (status code 200)
-                if image_response.status_code == 200:
-                    # Open a file with the filename to write the image content
-                    with open(filename, 'wb') as f:
-                        f.write(image_response.content)
-                    print(f"Image downloaded successfully as {filename}")
-                else:
-                    print(f"Failed to download image: HTTP status code {image_response.status_code}")
-            except requests.RequestException as e:
-                print(f"Failed to download image: {e}")
+                
